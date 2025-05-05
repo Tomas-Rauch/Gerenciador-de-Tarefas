7ab0b8144626480a7688ebf1994cc0f6ba281464
Tomas-Rauch <tomrauch11@gmail.com>
2025-05-05 00:42:50 -0300

    Ajustes finais na documentação do Swagger para a release 1.0

---
+++ src/docs/swagger.js
@@ -0,0 +1,31 @@
+const swaggerJsDoc = require('swagger-jsdoc');
+const swaggerUi = require('swagger-ui-express');
+
+const swaggerOptions = {
+  definition: {
+    openapi: '3.0.0',
+    info: {
+      title: 'API de Gerenciamento de Tarefas',
+      version: '1.0.0',
+      description: 'API para gerenciar tarefas com funcionalidades de criação, edição, exclusão e filtragem',
+      contact: {
+        name: 'Seu Nome',
+        email: 'seu.email@exemplo.com',
+      },
+    },
+    servers: [
+      {
+        url: 'http://localhost:3000',
+        description: 'Servidor de Desenvolvimento',
+      },
+    ],
+  },
+  apis: ['./src/routes/tarefaRoutes.js', './src/models/*.js', './src/controllers/*.js'],
+};
+
+const swaggerDocs = swaggerJsDoc(swaggerOptions);
+
+module.exports = {
+  swaggerUi,
+  swaggerDocs,
+};
